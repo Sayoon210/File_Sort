e9a9320fa4c57d628b330c995443e540aa4db2b6
IntrinsicPerson <sayoon210@gmail.com>
2020-06-10 18:40:34 +0900

    Update file.py

--- file.py
+++ file.py
@@ -2,7 +2,7 @@ import os
 import shutil
 path = "./"
 file_list = os.listdir(path)
-file_list_py = [file for file in file_list if file.find("2020142149") != -1]
+file_list_py = [file for file in file_list if file.find("~~~~~~~") != -1]
 
 for files in file_list_py:
-    shutil.move(files, '1학년 1학기')
+    shutil.move(files, '~~~~~')
